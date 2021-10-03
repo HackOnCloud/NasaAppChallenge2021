d323a7add95efd03907d9ba17f45520c26c32f55
Peter Phuc Le <peter.le.duong.cong@sg.ey.com>
2021-10-03 17:58:34 +0800

    Merge branch 'main' of https://github.com/HackOnCloud/NasaAppChallenge2021 into main
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- backend/SolarEnergyRecommendationLambdaFunction/src/SolarEnergyRecommendationLambdaFunction/Function.cs
+++ backend/SolarEnergyRecommendationLambdaFunction/src/SolarEnergyRecommendationLambdaFunction/Function.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Runtime.Serialization;
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -19,9 +22,104 @@ namespace SolarEnergyRecommendationLambdaFunction
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(Parameter input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            var result = new List<MonthObject>
+            {
+                new MonthObject
+                {
+                    month = "JAN",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$100"
+                },
+                new MonthObject
+                {
+                    month = "FEB",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$150"
+                },
+                new MonthObject
+                {
+                    month = "MAR",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$120"
+
+                },
+                new MonthObject
+                {
+                    month = "APR",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$110"
+                },
+                new MonthObject
+                {
+                    month = "MAY",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$120"
+                },
+                new MonthObject
+                {
+                    month = "JUN",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$120"
+                },
+                new MonthObject
+                {
+                    month = "JUL",
+                    angle = "19.0",
+                    energy = "5.04",
+                    orientation = "S",
+                    saving = "$120"
+                },
+                new MonthObject
+                {
+                    month = "AUG",
+                    angle = "7.5",
+                    energy = "4.6",
+                    orientation =  "S",
+                    saving = "$120"
+                },
+            };
+
+            var response = new APIGatewayProxyResponse
+            {
+                IsBase64Encoded = false,
+                StatusCode = (int)System.Net.HttpStatusCode.OK,
+                Body = Newtonsoft.Json.JsonConvert.SerializeObject(result),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+            };
+
+            return response;
+            //return Newtonsoft.Json.JsonConvert.SerializeObject(response);
         }
+    }
+
+    public class Parameter
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string provider { get; set; }
+        public string monthlybill { get; set; }
+    }
+
+    public class MonthObject
+    {
+        public string month { get; set; }
+        public string angle { get; set; }
+        public string energy { get; set; }
+        public string orientation { get; set; }
+        public string saving { get; set; }
     }
 }
